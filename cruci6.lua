2a213ee6a52f7adaa42fcea981e4cb56fd3bccc6
aad <92798216+aadyian9000@users.noreply.github.com>
2022-11-07 18:05:50 +0500

    Update cruci6.lua

--- cruci6.lua
+++ cruci6.lua
@@ -253,7 +253,7 @@ Creator.runEntity = function(entity)
                     -- Crucifix (lol)
                     
                     if Char:FindFirstChild("Crucifix") then
-                            print("gggggggggg")
+                            print("Wharr")
                         --Connections[entity.Model].Movement:Disconnect()
                         StopMovingDude = true
                             
